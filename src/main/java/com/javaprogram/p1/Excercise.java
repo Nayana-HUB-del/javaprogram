ddffa88a1d82abdfed460e5e9f2a80c8fe3ea8b6
hrnay <hrnay@Nayana>
2024-10-09 16:37:36 +0530

    My First Commit

---
+++ src/main/java/com/javaprogram/p1/Excercise.java
@@ -0,0 +1,10 @@
+package com.javaprogram.p1;
+
+public class Excercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("");
+	}
+
+}
---
+++ src/main/java/com/javaprogram/p1/TestCode.java
@@ -0,0 +1,10 @@
+package com.javaprogram.p1;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
